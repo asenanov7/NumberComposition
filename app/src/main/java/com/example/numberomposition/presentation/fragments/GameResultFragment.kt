650a142bcf3c45d311d6109a208cc4c83459a42a
asenanov7 <asenanov7@gmail.com>
2023-01-28 04:34:30 +0300

    adding navigation

--- app/src/main/java/com/example/numberomposition/presentation/fragments/GameResultFragment.kt
+++ app/src/main/java/com/example/numberomposition/presentation/fragments/GameResultFragment.kt
@@ -1,23 +1,19 @@
 package com.example.numberomposition.presentation.fragments
 
 import android.os.Bundle
-import android.text.Editable.Factory
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.numberomposition.R
 import com.example.numberomposition.databinding.FragmentResultBinding
-import com.example.numberomposition.domain.entity.GameResult
-import com.example.numberomposition.presentation.fragments.ChooseLevelFragment.Companion.GAME_FRAGMENT
-import com.example.numberomposition.presentation.fragments.WelcomeFragment.Companion.CHOOSE_LEVEL_FRAGMENT
 
 class GameResultFragment:Fragment() {
 
-    private lateinit var gameResult: GameResult
+    private val args by navArgs<GameResultFragmentArgs>()
 
     private var _binding:FragmentResultBinding? = null
     private val binding:FragmentResultBinding
@@ -30,26 +26,19 @@ class GameResultFragment:Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        parseArgs()
 
         setDrawable()
         bindingTextViews()
 
         binding.buttonRepeat.setOnClickListener { retryGame() }
 
-        //Слушатель клика у активити на кнопку назад
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true){
-            override fun handleOnBackPressed() {
-                retryGame()
-            }
-        })
     }
 
     private fun setDrawable(){
         val sadDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.icons8_sad_but_relieved_face_96 )
         val smileDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.icons8_smiling_face_with_smiling_eyes_96)
 
-        if (gameResult.winner) {
+        if (args.gameResult.winner) {
             binding.imageViewSmile.setImageDrawable(smileDrawable)
         }else {
             binding.imageViewSmile.setImageDrawable(sadDrawable)
@@ -57,20 +46,20 @@ class GameResultFragment:Fragment() {
     }
 
     private fun bindingTextViews(){
-        val neededPercent = gameResult.gameSettings.minPercentOfRightAnswers.toString()
+        val neededPercent = args.gameResult.gameSettings.minPercentOfRightAnswers.toString()
         binding.textViewNeededPercent.text =
             String.format(getString(R.string.neededPercentOfRightAnswers), neededPercent)
 
-        val neededPoints = gameResult.gameSettings.minCountOfRightAnswers.toString()
+        val neededPoints = args.gameResult.gameSettings.minCountOfRightAnswers.toString()
         binding.textViewCountOfNeededRightAnswers.text =
             String.format(getString(R.string.neededPoints), neededPoints)
 
-        val percent = (gameResult.countOfRightAnswers.toDouble()/
-                (gameResult.countOfQuestions-1).toDouble()*100).toString().substringBefore(".")
+        val percent = (args.gameResult.countOfRightAnswers.toDouble()/
+                (args.gameResult.countOfQuestions-1).toDouble()*100).toString().substringBefore(".")
         binding.textViewYourPercent.text =
             String.format(getString(R.string.yourPercentOfRightAnswers), percent)
 
-        val points =gameResult.countOfRightAnswers.toString()
+        val points = args.gameResult.countOfRightAnswers.toString()
         binding.textViewYourPoints.text =
             String.format(getString(R.string.yourPoints), points)
     }
@@ -80,24 +69,8 @@ class GameResultFragment:Fragment() {
         _binding = null
     }
 
-    private fun parseArgs(){
-        gameResult = arguments?.getParcelable<GameResult>(KEY_GAME_RESULT) as GameResult
-    }
-
     private fun retryGame(){
-        requireActivity().supportFragmentManager.popBackStack(GAME_FRAGMENT, 1)
-    }
-
-    companion object{
-        private const val KEY_GAME_RESULT ="gameResult"
-
-        fun getInstance(gameResult: GameResult):GameResultFragment{
-            return GameResultFragment().apply {
-                arguments = Bundle().apply {
-                    putParcelable(KEY_GAME_RESULT, gameResult)
-                }
-            }
-        }
+        findNavController().popBackStack()
     }
 
 }
